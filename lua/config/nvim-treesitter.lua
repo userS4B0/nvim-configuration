c9af6589dfce5f584368cdf95d1e962389644d0e
userS4B0 <userS4B0@proton.me>
2022-09-06 13:29:48 +0200

    Changed nvim-treesiter config

--- lua/config/nvim-treesitter.lua
+++ lua/config/nvim-treesitter.lua
@@ -1,17 +1,24 @@
-require'nvim-treesitter.configs'.setup {
+require('nvim-treesitter.configs').setup {
+  ensure_installed = {
+    "lua", 
+    "bash", 
+    "css", 
+    "html", 
+    "java", 
+    "javascript", 
+    "json", 
+    "latex",
+    "markdown", 
+    "python", 
+    "toml", 
+    "yaml"
+  },
+  sync_install = false,
+  auto_install = false,
+  ignore_install = {},
 
-    ensure_installed = {
-        "lua", "bash", "css", "html", "java", "javascript", "json", "latex",
-        "markdown", "python", "toml", "yaml"
-    },
-
-    sync_install = false,
-    auto_install = false,
-    ignore_install = {},
-
-    highlight = {
-        enable = true,
-        disable = {},
-        additional_vim_regex_highlighting = false
-    }
+  highlight = {
+    enable = true,
+    disable = {},  
+  }
 }
