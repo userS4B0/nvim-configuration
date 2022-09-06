9fd3aeeaf195d2194443b1870ff3260366e5be25
userS4B0 <userS4B0@proton.me>
2022-09-06 11:09:31 +0200

    Added git integration plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,44 +1,60 @@
 -- [[ plug.lua ]]
 return require('packer').startup(function(use)
 
-    -- [[ General ]]
-    use {'wbthomason/packer.nvim'}
-    use {'tpope/vim-commentary'}
+  -- [[ General ]] ------------------------------------------------------------------------------
+  
+    -- Plugin Manager
+  use {'wbthomason/packer.nvim'}
 
-    use {
-        'kyazdani42/nvim-tree.lua',
-        requires = {'kyazdani42/nvim-web-devicons', opt = true}
-    }
+    -- Coments
+  use {'tpope/vim-commentary'}
+  
+    -- File Tree
+  use {'kyazdani42/nvim-tree.lua',
+       requires = {'kyazdani42/nvim-web-devicons', opt = true}}
+  
+
+  -- [[ Theming ]] ------------------------------------------------------------------------------
+  
+    -- Theme
+  use {'folke/tokyonight.nvim'}
+  
+    -- Dashboard
+  use {'glepnir/dashboard-nvim', config = [[require('config.dashboard-nvim')]]}
     
-    -- [[ Theming ]]
-    use {
-        'glepnir/dashboard-nvim',
-        config = [[require('config.dashboard-nvim')]]
-    }
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = {'kyazdani42/nvim-web-devicons', opt = true},
-        config = [[require('config.lualine')]]
-    }
-    use {'folke/tokyonight.nvim'}
+    -- Status Line
+  use {'nvim-lualine/lualine.nvim',
+       requires = {'kyazdani42/nvim-web-devicons', opt = true},
+       config = [[require('config.lualine')]]
+  }
+ 
+    -- Tabs & Buffers
+  use {'akinsho/bufferline.nvim', 
+       tag = "v2.*", 
+       requires = {'kyazdani42/nvim-web-devicons', opt = true}
+  }
 
-    use {
-        'akinsho/bufferline.nvim',
-        tag = "v2.*",
-        requires = {'kyazdani42/nvim-web-devicons', opt = true}
-    }
-
-    -- [[ Development ]]
-    use {'windwp/nvim-autopairs'}
-    use {'nvim-treesitter/nvim-treesitter'}
+  
+  -- [[ Development ]] --------------------------------------------------------------------------
     
-    use {
-      'nvim-telescope/telescope.nvim', 
-      tag = '0.1.0',
-      requires = {
-        {'nvim-lua/plenary.nvim'},
-        {'kyazdani42/nvim-web-devicons', opt = true}
-      }
-    }
+    -- Autopairing characters (){}[]''""
+  use {'windwp/nvim-autopairs'}                                 
+  
+    -- Better syntax highlighting
+  use {'nvim-treesitter/nvim-treesitter'}
+    
+    -- Fuzzy file finder
+  use {'nvim-telescope/telescope.nvim', 
+       tag = '0.1.0',
+       requires = {{'nvim-lua/plenary.nvim'}, 
+                  {'kyazdani42/nvim-web-devicons', opt = true}
+       }
+  }
+
+    -- Git integration
+  use {'tpope/vim-fugitive'}
+
+    -- Code completion
+  -- use {'neoclide/coc.nvim'}  
+
 end)
-
--- lua/plugrc.lua
+++ lua/plugrc.lua
@@ -3,6 +3,5 @@ require('config.lualine')
 require('config.nvim-tree')
 require('nvim-autopairs').setup {}
 require('config.bufferline')
--- require('bufferline').setup{}
 require('config.nvim-treesitter')
 require('telescope')
