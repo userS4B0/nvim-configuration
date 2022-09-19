32d74e5449760b9af95fd3106669939a61feb110
userS4B0 <userS4B0@proton.me>
2022-09-19 22:13:33 +0200

    minor changes

--- lua/config/autopairs.lua
+++ lua/config/autopairs.lua
@@ -1,1 +1,16 @@
-require("nvim-autopairs").setup{}
+-- require("nvim-autopairs").setup{}
+local status_ok, autopairs = pcall(require, "nvim-autopairs")
+if not status_ok then
+  return
+end
+
+autopairs.setup({
+  enable_check_bracket_line = false, -- Don't add pairs if it already has a close pair in the same line
+  ignored_next_char = "[%w%.]", -- will ignore alphanumeric and `.` symbol
+  check_ts = true, -- use treesitter to check for a pair.
+  ts_config = {
+    lua = { "string" }, -- it will not add pair on that treesitter node
+    javascript = { "template_string" },
+    java = false, -- don't check treesitter on java
+  },
+})
