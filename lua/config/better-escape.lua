8c86186f3530fcee13f4e4ab4be4a48845fc2801
userS4B0 <userS4B0@proton.me>
2022-09-19 16:15:01 +0200

    minimal changes

--- lua/config/better-escape.lua
+++ lua/config/better-escape.lua
@@ -1,3 +1,8 @@
+local status_ok, better_escape = pcall(require, "better-escape")
+if not status_ok then
+  return
+end
+
 require('better_escape').setup {
   mapping = {'jj', 'kk'},
   timeout = vim.o.timeoutlen,
