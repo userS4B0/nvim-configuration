9ede6ab3c11d44ef65f3fe96f60bb657b3268b2b
userS4B0 <userS4B0@proton.me>
2022-09-19 13:31:59 +0200

    added some keybindings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,10 +1,68 @@
 -- [[ keys.lua ]]
+
 local map = vim.api.nvim_set_keymap
+local opts = {noremap = true, silent = true}
 
--- General keybindings
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
 
---- Plugin keybindings
 
--- BufferLine
-map('n', '.', [[:BufferLineCycleNext]], {})
-map('n', ',', [[:BufferLineCyclePrev]], {})
+-- Normal --
+-- Other shortcuts
+map("n", "<C-s>", ":w<CR>", opts)
+map("n", "<C-x>", ":qa!<CR>", opts)
+
+-- Better window navigation
+map("n", "<C-h>", "<C-w>h", opts)
+map("n", "<C-j>", "<C-w>j", opts)
+map("n", "<C-k>", "<C-w>k", opts)
+map("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize with arrows
+map("n", "<C-Up>", ":resize -2<CR>", opts)
+map("n", "<C-Down>", ":resize +2<CR>", opts)
+map("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+map("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+map("n", "<S-l>", ":bnext<CR>", opts)
+map("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Nvimtree
+map("n", "t", ":NvimTreeToggle<cr>", opts)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- LuaTree
+-- map("n", "t", function()
+--   return require("nvim-tree").toggle(false, true)
+-- end, {noremap = true, silent = true, desc = "toggle nvim-tree"})
+
+-- -- map("n", ",", [[:BufferLinecyclePrev]], {noremap = true, silent = true, desc = "goto prev buffer"})
+-- map("n", ".", function()
+--   return require("bufferline").go_to_buffer(1, false)
+-- end, {noremap = true, silent = true, desc = "goto next buffer"})
+
+-- map("n", ",", function()
+--   return require("bufferline").go_to_buffer(-1, false)
+-- end, {noremap = true, silent = true, desc = "goto prev buffer"})
