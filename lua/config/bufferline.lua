9d1f96f0d6a22abb69625fc6d98c8a19758b91f9
userS4B0 <userS4B0@proton.me>
2022-09-19 13:11:30 +0200

    changed some bufferline configuration

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -1,48 +1,41 @@
-require("bufferline").setup {
+local status_ok, bufferline = pcall(require, "bufferline")
+if not status_ok then
+  return
+end
+
+bufferline.setup {
   options = {
-    numbers = "buffer_id",
-    close_command = "bdelete! %d",
-    right_mouse_command = nil,
+    numbers = "none",
+    close_command = "Bdelete! %d",
+    right_mouse_command = "Bdelete! %d",
     left_mouse_command = "buffer %d",
     middle_mouse_command = nil,
-    
     indicator = {
       icon = '▎', -- this should be omitted if indicator style is not 'icon'
       style = 'icon'
     },
-    
     buffer_close_icon = "",
     modified_icon = "●",
     close_icon = "",
     left_trunc_marker = "",
     right_trunc_marker = "",
-    max_name_length = 18,
-    max_prefix_length = 15,
-    tab_size = 10,
-    diagnostics = false,
-    
-    custom_filter = function(bufnr)
-      -- if the result is false, this buffer will be shown, otherwise, this
-      -- buffer will be hidden.
-
-      -- filter out filetypes you don't want to see
-      local exclude_ft = {"qf", "fugitive", "git"}
-      local cur_ft = vim.bo[bufnr].filetype
-      local should_filter = vim.tbl_contains(exclude_ft, cur_ft)
-
-      if should_filter then return false end
-
-      return true
-    end,
-
+    max_name_length = 30,
+    max_prefix_length = 30,    tab_size = 21,
+    diagnostics = "coc",
+    diagnostics_update_in_insert = true,
+    offsets = {{
+      filetype = "NvimTree",
+      text = "File Explorer",
+      text_align = "center",
+      separator = true,
+    }},
     show_buffer_icons = false,
     show_buffer_close_icons = true,
     show_close_icon = true,
     show_tab_indicators = true,
-    persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-    separator_style = "bar",
-    enforce_regular_tabs = false,
+    persist_buffer_sort = true,
+    separator_style = "thin",
+    enforce_regular_tabs = true,
     always_show_bufferline = true,
-    sort_by = "id"
-  }
+  },
 }
