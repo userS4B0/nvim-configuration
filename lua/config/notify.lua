16e8b67ce442d0e2878e3b318b57888c28b644fa
userS4B0 <userS4B0@proton.me>
2022-09-19 23:24:58 +0200

    added notify configuration

--- lua/config/notify.lua
+++ lua/config/notify.lua
@@ -3,4 +3,20 @@ if not status_ok then
   return
 end
 
-notify.setup{}
+notify.setup({
+  stages            = "fade_in_slide_out",  -- Animation style
+  on_open           = nil,                  -- Func when new window is opened
+  on_close          = nil,                  -- Func when window is closed
+  render            = "default",            -- Render function for notifications. 
+  timeout           = 5000,                 -- Default timeout
+  background_colour = "#000000",            -- Notification background
+  minimum_width     = 50,                   -- Minimum width for notification windows 
+  icons = {                                 -- Icons for the different levels
+    ERROR           = "",
+    WARN            = "",
+    INFO            = "",
+    DEBUG           = "",
+    TRACE           = "✎"
+    }
+})
+vim.notify = require("notify")
