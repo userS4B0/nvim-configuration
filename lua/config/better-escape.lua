9a0185734fec9d6e1b8d58596250a892406d7c4a
userS4B0 <userS4B0@proton.me>
2022-09-19 21:20:43 +0200

    minimal changes

--- lua/config/better-escape.lua
+++ lua/config/better-escape.lua
@@ -1,11 +1,12 @@
-local status_ok, better_escape = pcall(require, "better-escape")
+local status_ok, better_escape = pcall(require, "better_escape")
 if not status_ok then
   return
 end
 
-require('better_escape').setup {
-  mapping = {'jj', 'kk'},
+better_escape.setup {
+  
+  mapping = {"jj", "kk"},
   timeout = vim.o.timeoutlen,
   clear_empty_lines = false,
-  keys = '<Esc>'
+  keys = "<Esc>"
 }
