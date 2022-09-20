71e83b5396605b91e6fcc9746ccf6d004c7638a5
userS4B0 <userS4B0@proton.me>
2022-09-20 17:11:58 +0200

    changed dashboard config

--- lua/config/dashboard.lua
+++ lua/config/dashboard.lua
@@ -15,14 +15,10 @@ dashboard.custom_header = {
 }
 
 dashboard.custom_center = {
-  {icon = " ﱐ ", desc = "  Find File ", action = ":Telescope find_files<CR>"},
-  {icon = "  ", desc = "  NeoVim Settings ", action = ":e $MYVIMRC"},
-  {icon = "  ", desc = "  Recent Files ", action = ":Telescope oldfiles<CR>"},
+  -- {icon = " ﱐ ", desc = "  Find File ", action = "Telescope find_files<CR>"},
+  -- TODO: Find File & Recent files not working
+  {icon = "  ", desc = "  NeoVim Settings ", action = ":e $MYVIMRC | :cd %:p:h <CR>"},
+  -- {icon = "  ", desc = "  Recent Files ", action = "Telescope oldfiles<CR>"},
   {icon = "  ", desc = "  Exit NeoVim ", action = "qa"}
 }
 
-vim.cmd([[
-  augroup dashboard_enter
-    au!
-  augroup END
-]])
