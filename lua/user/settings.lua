f5779c48b7aa1fe202c574fed3be40c4e0457231
userS4B0 <userS4B0@proton.me>
2022-09-26 15:48:17 +0200

    cleaned options

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -1,44 +1,34 @@
 -- [[ neovim settings ]]
+-- See :h options
+
 local set = vim.opt
 
 -- [[ General ]]
 
-set.backup = false                      -- Creates a backup file
 set.clipboard = "unnamedplus"           -- Uses system clipboard
-
--- [[ Context ]]
+set.encoding = "utf8"		                -- String encoding to use
+set.fileencoding = "utf8"	              -- File encoding to use
+set.mouse = "a"                         -- Mouse integration
+set.showmode = false                    -- Hide (--INSERT--), (--VISUAL--) ; etc...
 set.colorcolumn = "100"                 -- Show col for max line length
 set.number = true		                    -- Show line numbers
 set.relativenumber = true	              -- Show relative line numbers
 set.scrolloff = 4		                    -- Min num lines of context
 set.signcolumn = "yes"		              -- Show the sign column
-set.mouse = "a"                         -- Mouse integration
-set.cmdheight = 1                       -- More space for nvim cli to display msgs
-set.showmode = false                    -- Hide (--INSERT--), (--VISUAL--) ; etc...
-
--- [[ Filetypes ]]
-set.encoding = "utf8"		                -- String encoding to use
-set.fileencoding = "utf8"	              -- File encoding to use
-
--- [[ Theme ]]
 set.syntax = "ON"		                    -- Allow syntax highlighting
 set.termguicolors = true	              -- If term supports ui color then enable
 
--- [[ Search ]]
+-- Window
+set.splitright = true		                -- Place new window to right of the current one
+set.splitbelow = true		                -- Place a new window below the current one
+set.wrap = false                        -- No text wrapping
+
+-- Search
 set.ignorecase = true		                -- Ignore case in search
 set.smartcase = true		                -- Override ignorecase if search contains capitals
-set.incsearch = true		                -- Use incremental search
-set.hlsearch = true		                  -- Highlight search matches
 
--- [[ Whitespace ]]
+-- Files
 set.expandtab = true		                -- Use spaces instead of tabs
 set.shiftwidth = 2		                  -- Size of an indent
 set.softtabstop = 2		                  -- Number of spaces tabs count for insert mode
 set.tabstop = 2			                    -- Number of spaces tabs count for
-
--- [[ Splits ]]
-set.splitright = true		                -- Place new window to right of the current one
-set.splitbelow = true		                -- Place a new window below the current one
-
--- [[ Window ]]
-set.wrap = false                        -- No text wrapping
